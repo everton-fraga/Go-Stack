3eed8a1298121c4db904b48aa6b7f219f2ffae80
Everton Fraga <evertonfraga@MacBook-Pro-de-Everton.local>
2020-05-13 10:36:38 -0300

    Adequações para continuidade do projeto Gobarber

---
+++ modulo03.1/src/config/auth.js
@@ -0,0 +1,6 @@
+export default {
+  secret: process.env.APP_SECRET,
+  expiresIn: '70d',
+};
+// Obtido no site: www.md5online.org
+// The MD5 hash for postbirdgenevamd5opswatjointoday is : 7f03c1ab0415821816695a36156957f0
